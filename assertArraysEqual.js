9a89e5479483ae6908b449ac97b7bbc2cbb7e663
Robbie Thomas <robbiekt@gmail.com>
2023-01-10 15:07:19 -0800

    completed asserArraysEqual function

---
+++ assertArraysEqual.js
@@ -0,0 +1,25 @@
+const eqArrays = function (array1, array2) {
+  if (array1.length !== array2.length ) {
+   return false;
+  }
+   for (let i = 0; i < array1.length; i++) {
+     if (array1[i] !== array2[i]) {
+       return false
+     }
+   }
+   return true;
+ }
+
+ const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}✅✅✅ `);
+  } else {
+    console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}🛑🛑🛑`);
+  }
+};
+
+
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
+assertArraysEqual([1, 2, 3], [3, 2, 1]);
+assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]);
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3]);
