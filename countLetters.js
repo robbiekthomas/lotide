2b1f12a5fd1bb8d3777b02008cfe6da48fa94223
Robbie Thomas <robbiekt@gmail.com>
2023-01-11 12:06:53 -0800

    created countLetters function

---
+++ countLetters.js
@@ -0,0 +1,31 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}✅✅✅ `);
+  } else {
+    console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}🛑🛑🛑`);
+  }
+};
+
+const countLetters = function(letters) {
+  let answer = {};
+  for (let letter of letters) {
+    let charCode = letter.charCodeAt(0);
+    if ((charCode >= 65 && charCode < 91) || (charCode >= 97 && charCode < 123)) {
+      answer[letter] ? answer[letter] += 1: answer[letter] = 1;
+    }
+  }
+  return answer;
+}
+
+const result = countLetters("lighthouse in the house");
+
+console.log(assertEqual(result['l'], 1));
+console.log(assertEqual(result['i'], 2));
+console.log(assertEqual(result['g'], 1));
+console.log(assertEqual(result['h'], 4));
+console.log(assertEqual(result['t'], 2));
+console.log(assertEqual(result['o'], 2));
+console.log(assertEqual(result['u'], 2));
+console.log(assertEqual(result['s'], 2));
+console.log(assertEqual(result['e'], 3));
+console.log(assertEqual(result['n'], 1));
