f38c181f85ad7e896a936b88b9a3daa1bcc6a3cb
Robbie Thomas <robbiekt@gmail.com>
2023-01-16 08:23:39 -0800

    added test file for eqArrays

--- eqArrays.js
+++ eqArrays.js
@@ -1,25 +1,13 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}✅✅✅ `);
-  } else {
-    console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}🛑🛑🛑`);
-  }
-};
-
-
 const eqArrays = function (array1, array2) {
- if (array1.length !== array2.length ) {
-  return false;
- }
+  if (array1.length !== array2.length) {
+    return false;
+  }
   for (let i = 0; i < array1.length; i++) {
     if (array1[i] !== array2[i]) {
-      return false
+      return false;
     }
   }
   return true;
-}
+};
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+module.exports = eqArrays;
