fd587d7fe362ffd664644aef3d01e600f616a82a
Robbie Thomas <robbiekt@gmail.com>
2023-01-16 08:44:42 -0800

    added mocha and chai and updated headTest

--- test/assertEqualTest.js
+++ test/assertEqualTest.js
@@ -1,6 +1,6 @@
 const assertEqual = require("../assertEqual");
 
-assertEqual("Lighthouse Labs", "Bootcamp");
+// assertEqual("Lighthouse Labs", "Bootcamp");
 assertEqual("Lighthouse Labs", "Lighthouse Labs");
 assertEqual(1, 1);
-assertEqual(1, 2);
+// assertEqual(1, 2);
