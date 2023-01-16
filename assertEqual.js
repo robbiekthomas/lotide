1c01271dd4e84a1fa5adf5e18ab6b928ed3f011c
Robbie Thomas <robbiekt@gmail.com>
2023-01-16 08:18:57 -0800

    exported assertEqual and added test files for head

--- assertEqual.js
+++ assertEqual.js
@@ -1,13 +1,9 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}✅✅✅ `);
-  } else {
+const assertEqual = function (actual, expected) {
+  if (actual !== expected) {
     console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}🛑🛑🛑`);
+    return;
   }
+  console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}✅✅✅ `);
 };
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual("Lighthouse Labs", "Lighthouse Labs");
-assertEqual(1,1);
-assertEqual(1,2);
+module.exports = assertEqual;
--- head.js
+++ head.js
@@ -1,16 +1,5 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}✅✅✅ `);
-  } else {
-    console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}🛑🛑🛑`);
-  }
+const head = function (array) {
+  return array[0];
 };
 
-const head = function(array) {
-  return array[0];
-}
-
-assertEqual(head([5,6,7]), 5);
-assertEqual(head([]), undefined);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head(["Hello"]), "Hello");
+module.exports = head;
